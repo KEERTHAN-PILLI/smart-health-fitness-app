e3d1f16645a10914c4214fc381a9fc746fe5be0d
KEERTHAN-PILLI <keerukeeru5@gmail.com>
2026-02-07 13:53:30 +0530

    Use cloud MySQL for production

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,10 +1,12 @@
 import mysql from "mysql2";
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "9392413065",
-  database: "smart_health_fitness"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: true }
 });
 
 export default db;
