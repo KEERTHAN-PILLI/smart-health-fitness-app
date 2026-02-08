5606ca76991da32b2562217e246f488e0b848dd3
KEERTHAN-PILLI <keerukeeru5@gmail.com>
2026-02-08 20:01:17 +0530

    Update Errors

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
-import { HashRouter } from "react-router-dom";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/smart-health-fitness-app">
-    <HashRouter>
-  <AuthProvider>
-    <App />
-  </AuthProvider>
-</HashRouter>
-  </BrowserRouter>
-  
+  <HashRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </HashRouter>
 );
